fc4177af670bca844bb4a3951f79a5aa3c8d1035
Moustafa Ismail <moustafa.ismail@robustastudio.com>
2023-03-18 12:38:33 +0200

    add more selenium commands

--- src/test/java/T04_JavascriptAlert.java
+++ src/test/java/T04_JavascriptAlert.java
@@ -1,2 +1,12 @@
 public class T04_JavascriptAlert {
+
+//      URL: https://the-internet.herokuapp.com/javascript_alerts -> switchTo, Class Alert
+//      URL: https://the-internet.herokuapp.com/digest_auth -> "https://username:password@url"
+
+// Alert Box
+
+// Confirm Box
+
+// Prompt Box
+
 }
---
+++ src/test/java/T05_WindowHandling.java
@@ -0,0 +1,12 @@
+
+public class T05_WindowHandling {
+
+//      Don't apply Thread.sleep -> it is for you to see the execution ; remove it once you learn the script's purpose
+//      URL: https://demo.nopcommerce.com/
+/*
+        ArrayList<String> tabs = new ArrayList<>(driver.getWindowHandles());
+        driver.switchTo().window(tabs.get(1));
+ */
+
+
+}
